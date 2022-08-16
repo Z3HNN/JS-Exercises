399201432b0aa184886b2f3e9a3f564d6475ccfc
Zehn <z3hndolphin@gmail.com>
2022-08-16 18:15:09 -0400

    p

--- exercise.js
+++ exercise.js
@@ -1,3 +1,8 @@
-const isEqualTo100 = (a,b) => a === 100 || b === 100;
+const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b) === 100;
 console.log(isEqualTo100(100,0))
 console.log(isEqualTo100(0,100))
+
+const getFileExtension = (str) => str.slice
+(str.lastIndexOf('.'))
+console.log(getFileExtension('index.html'))
+c
